a621081b1228e182e73fdf8d35c7e3387c614896
Karim Ratib <karim.ratib@gmail.com>
2021-01-28 18:11:07 -0800

    Update to jekyll 4.2.0

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ ruby "2.7.2"
 #
 # This will help ensure the proper Jekyll version is running.
 # Happy Jekylling!
-gem "jekyll", "3.9.0"
+gem "jekyll", "4.2.0"
 
 # This is the default theme for new Jekyll sites. You may change this to anything you like.
 gem "minima", "~> 2.0"
@@ -27,7 +27,7 @@ gem 'ruby-oembed', '~> 0.14.0'
 group :jekyll_plugins do
    gem "jekyll-feed", "~> 0.6"
    gem "jemoji"
-   gem "jekyll-font-awesome-sass"
+   gem 'jekyll-fontawesome-svg', '~> 0.3.4'
    gem "jekyll-seo-tag"
    gem "jekyll-extlinks"
 end
--- _plugins/oembed.rb
+++ _plugins/oembed.rb
@@ -4,18 +4,21 @@ module Jekyll
   class OEmbedTag < Liquid::Tag
     def initialize(tag_name, content, tokens)
       super
-      @content = content
+      @content = content.strip
+      @@cache ||= Jekyll::Cache.new("OEmbedTag")
       OEmbed::Providers.register_all
       OEmbed::Providers.register_fallback(OEmbed::ProviderDiscovery, OEmbed::Providers::Noembed)
     end
 
     def render(context)
-      begin
-        resource = OEmbed::Providers.get(@content.strip)
-        resource.html
-      rescue StandardError => e
-        Jekyll.logger.error("oEmbed:", "Could not extract oEmbed information from #{@content.strip}: #{e}")
-        ""
+      @@cache.getset(@content) do
+        begin
+          resource = OEmbed::Providers.get(@content)
+          resource.html
+        rescue StandardError => e
+          Jekyll.logger.error("oEmbed:", "Could not extract oEmbed information from #{@content}: #{e}")
+          ""
+        end
       end
     end
 
